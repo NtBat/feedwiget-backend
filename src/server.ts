d6e33a144087c666d432dc1beb435fd8c853436c
NtBat <na.than.fjv@gmail.com>
2022-05-09 18:19:34 -0300

    created send mail

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from "nodemailer";
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "71d709450fdd98",
+    pass: "e46d6922020640"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const {type, comment, screenshot} = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: "Equipe Feedback Widget <oi@feedget.com>",
+    to: "Nathan Batista <contato@ntbatista.dev>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({data: feedback});
